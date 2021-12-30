5fcdc2c33b4b6643541cf7ff00115f6fec094511
Arnav <96633363+ArnavJainS@users.noreply.github.com>
2021-12-29 21:57:28 -0700

    Create login2.js

---
+++ login2.js
@@ -0,0 +1,17 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="arnavjain711@gmail.com"&& password=="Amazing12$")
+{
+    alert("login succesfully");
+    return false;
+
+}
+else
+{
+    alert("login failed");
+}
+
+
+}
